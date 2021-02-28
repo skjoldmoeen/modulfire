02a2324aab04e8ab19a8655deacbff022546c73c
Ingibjorg Sigfusdottir <ingibjorgesigfusd@hotmail.com>
2021-02-28 18:47:43 +0100

    tilbageknap fixed og 1=grøn

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const previousBtn = document.getElementById('previousBtn');
 const nextBtn = document.getElementById('nextBtn');
-const content = document.getElementById('content');
+
 const  bullets = [...document.querySelectorAll('.bullet')];
 
 const MAX_STEPS = 6;
 let currentStep = 1;
+
 
 nextBtn.addEventListener('click', () => {
         const currentBullet = bullets[currentStep - 1 ];
         currentBullet.classList.add('completed');
         currentStep++;
         previousBtn.disabled = false;
-        
+
     });
 
 previousBtn.addEventListener('click', () => {
@@ -22,4 +23,6 @@ previousBtn.addEventListener('click', () => {
     if (currentStep === 1){
         previousBtn.disabled = true;
     }
+    
+
 })
