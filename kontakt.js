6f33cbfab6b732502c1c4d70b3579f468dbec194
dittekirk <ditteemil@gmail.com>
2021-03-04 21:36:17 +0100

    Create kontakt.js

---
+++ kontakt.js
@@ -0,0 +1,21 @@
+function validateForm() {
+  var x = document.forms["myForm"]["Name"].value;
+  if (x == "") {
+    alert("Felter markeret med * skal udfyldes");
+    return false;
+  }
+}
+function validateForm() {
+  var x = document.forms["myForm"]["your-email"].value;
+  if (x == "") {
+    alert("Felter markeret med * skal udfyldes");
+    return false;
+  }
+}
+function validateForm() {
+  var x = document.forms["myForm"]["message"].value;
+  if (x == "") {
+    alert("Felter markeret med * skal udfyldes");
+    return false;
+  }
+}
